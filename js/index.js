6d29ef188f7c372dbe9db6664e91c038ab15b8f1
UriShX <usdogi@gmail.com>
2020-11-10 13:43:52 +0200

    start Snowpack integration

---
+++ js/index.js
@@ -0,0 +1,16 @@
+import 'three/build/three';
+import 'three/examples/js/controls/DragControls';
+import 'three/examples/js/controls/OrbitControls';
+import 'three/examples/js/controls/TransformControls';
+import 'three/examples/js/exporters/STLExporter';
+import 'three/examples/js/exporters/OBJExporter';
+import 'controlkit/lib/ControlKit';
+import 'jquery/dist/jquery';
+import 'golden-layout/dist/goldenlayout';
+import 'golden-layout/src/css/goldenlayout-base.css';
+import 'golden-layout/src/css/goldenlayout-dark-theme.css';
+import './MainPage/CascadeViewHandles'
+import './MainPage/CascadeView'
+
+<script type="text/javascript" src="./node_modules/rawflate/rawdeflate.js"></script>
+<script type="text/javascript" src="./node_modules/rawflate/rawinflate.js"></script>
